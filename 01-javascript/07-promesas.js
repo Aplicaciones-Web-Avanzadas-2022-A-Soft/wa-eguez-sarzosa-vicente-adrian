9305ef2ba7ea8f514ba0141da5b84468e28ac3a4
Adrian Eguez <eadepto@hotmail.com>
2022-05-30 08:57:15 -0500

    hasta async await

--- 01-javascript/07-promesas.js
+++ 01-javascript/07-promesas.js
@@ -17,10 +17,25 @@ function promesaEsPar(numero) {
     return miPrimerPromesa
 }
 
-promesaEsPar(2)
+function promesaElevarAlCuadrado(numero) {
+    const miPrimerPromesa = new Promise( // Definicion de la promesa
+        (resolve, reject) => {
+            const numeroElevadoAlCuadrado = Math.pow(numero, 2);
+            resolve(numeroElevadoAlCuadrado); // return numeroElevadoAlCuadrado
+        }
+    );
+    return miPrimerPromesa
+}
+
+promesaEsPar(4)
     .then( // return
-        (respuesta)=>{
-            console.log('Respuesta: ', respuesta);
+        (numeroPar)=>{
+            return promesaElevarAlCuadrado(numeroPar)// OTRA PROMESA
+        }
+    )
+    .then( // return
+        (numeroParElevadoAlCuadrado)=>{
+            console.log('numeroParElevadoAlCuadrado', numeroParElevadoAlCuadrado)
         }
     )
     .catch( // throw
