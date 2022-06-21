3b16be742073799aa8bfa1188fe42a0071db0e79
Adrian Eguez <eadepto@hotmail.com>
2022-06-21 07:19:17 -0500

    update/Hasta estilos con modulos css

--- 03-react/react-nextjs/components/hooks/useSelectMoneda.tsx
+++ 03-react/react-nextjs/components/hooks/useSelectMoneda.tsx
@@ -1,43 +1,43 @@
 import {useState} from 'react';
+import {MonedasInterface} from "../constantes/monedas";
 
-const useSelectMoneda = () => {
-    // Usamos Hooks React (useState)
+const useSelectMoneda = (label:string, opciones: MonedasInterface[]) => {
+    // Usamos Hooks React (userState)
     // const [state, setState] = useState('VALOR')
-    const [valorEjemplo, setValorEjemplo] = useState('Adrian Eguez');
-
+    const [valorEjemplo, setValorEjemplo] = useState('Christian Moran')
+    const generarSelectMonedas = () => {
+        return opciones.map((moneda) =>(
+                <option id={moneda.id} value={moneda.id}> {moneda.nombre}</option>
+            )
+        )
+    }
     const SelectMonedas = () => (
         <>
-            <label className="form-label" htmlFor="moneda">Moneda</label>
-            <select className="form-select" name="moneda" id="moneda">
-                <option value=""> Seleccione opcion</option>
+            <label className="form-label" htmlFor={label}>{label}</label>
+            <select className="form-select" name={label} id={label}>
+                <option value="">Seleccione opción</option>
+                {generarSelectMonedas()}
             </select>
-            <label className="form-label" htmlFor="moneda">Moneda</label>
-
-            <label className="form-label"
-                   htmlFor="ejemplo">
-                Ejemplo
-            </label>
-            <p>El valor es: {valorEjemplo}</p>
-            <input className="form-control"
-                   value={valorEjemplo}
-                   id="ejemplo"
-                   onChange={(e)=>{
-                       e.preventDefault();
-                       setValorEjemplo(e.target.value);
-                   }}
-                   type="text"/>
-            <button onClick={
-                (event)=>{
-                    event.preventDefault();
-                    // valorEjemplo = 'Vicente'; ESTO NO SIRVE
-                    setValorEjemplo('Vicente' + new Date().getTime());
-                }
-            }> Cambiar ejemplo </button>
-
-
+            {/*<label className="form-label" htmlFor="moneda">Moneda </label>*/}
+            {/*<label className="form-label" htmlFor="ejemplo">Ejemplo </label>*/}
+            {/*<p>El valor es: {valorEjemplo}</p>*/}
+            {/*<input className="form-control"*/}
+            {/*       value={valorEjemplo}*/}
+            {/*       id="ejemplo"*/}
+            {/*       onChange={(e)=>{*/}
+            {/*           e.preventDefault();*/}
+            {/*           setValorEjemplo(e.target.value)*/}
+            {/*       }}*/}
+            {/*       type="text"/>*/}
+            {/*<button onClick={*/}
+            {/*    (event)=>{*/}
+            {/*        event.preventDefault();*/}
+            {/*        setValorEjemplo('Christian '+new Date().getTime());*/}
+            {/*    }*/}
+            {/*}*/}
+            {/*>Cambiar ejemplo</button>*/}
         </>
     )
-
     return [SelectMonedas]
     // return [
     //     'hola',
