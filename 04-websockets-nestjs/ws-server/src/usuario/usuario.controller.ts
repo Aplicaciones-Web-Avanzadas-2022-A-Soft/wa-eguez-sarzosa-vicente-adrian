184d08f6a251b7602995db6f6a61512950216891
Adrian Eguez <eadepto@hotmail.com>
2022-08-16 08:46:02 -0500

    hasta usuario controller

--- 04-websockets-nestjs/ws-server/src/usuario/usuario.controller.ts
+++ 04-websockets-nestjs/ws-server/src/usuario/usuario.controller.ts
@@ -1,5 +1,19 @@
-import {Controller, Get, HttpCode, HttpStatus, Param, Query} from "@nestjs/common";
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Param,
+    Post,
+    Put,
+    Query
+} from "@nestjs/common";
 import {UsuarioService} from "./usuario.service";
+import {UsuarioCreateDto} from "./dto/usuario-create.dto";
+import {validate} from "class-validator";
 
 @Controller('usuario')
 export class UsuarioController {
@@ -34,6 +48,60 @@ export class UsuarioController {
         @Query() queryParams,
         @Param() params
     ){
-        return this.usuarioService.findOneById(+params.id);
+        return this.usuarioService.findOneById(+params.id); // +"1" = 1
     }
+
+    @Post("/") // POST /usuario
+    @HttpCode(201)
+    async create(
+        @Query() queryParams,
+        @Param() params,
+        @Body() bodyParams
+    ){
+        const nuevoRegistro = new UsuarioCreateDto(); // creamos dto
+        nuevoRegistro.rol = bodyParams.rol;
+        nuevoRegistro.nombres = bodyParams.nombres;
+        nuevoRegistro.apellidos = bodyParams.apellidos;
+
+        const errores = await validate(nuevoRegistro); // validamos
+        if(errores.length > 0){
+            console.error({errores});
+            throw new BadRequestException({mensaje:'Envio mal datos'});
+        }
+        // Creamos
+        return this.usuarioService.create(nuevoRegistro);
+    }
+
+    @Put("/:id") // PUT /usuario/:id
+    @HttpCode(200)
+    update(
+        @Query() queryParams,
+        @Param() params,
+        @Body() bodyParams
+    ){
+        return this.usuarioService.update(
+            bodyParams,
+            +params.id
+        );
+    }
+
+    @Delete("/:id") // DELETE /usuario/:id
+    @HttpCode(200)
+    delete(
+        @Query() queryParams,
+        @Param() params,
+        @Body() bodyParams
+    ){
+        return this.usuarioService.delete(+params.id);
+    }
+
+
+
+
+
+
+
+
+
+
 }
--- 04-websockets-nestjs/ws-server/src/usuario/usuario.module.ts
+++ 04-websockets-nestjs/ws-server/src/usuario/usuario.module.ts
@@ -2,6 +2,7 @@ import {Module} from "@nestjs/common";
 import {UsuarioService} from "./usuario.service";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {UsuarioEntity} from "./usuario.entity";
+import {UsuarioController} from "./usuario.controller";
 
 // Modulo Usuario
 @Module({
@@ -9,10 +10,13 @@ import {UsuarioEntity} from "./usuario.entity";
         TypeOrmModule.forFeature(
             [UsuarioEntity], // Entidad en este modulo
             'default' //  nombre de la conexion
-        )
+        ),
     ],
     providers: [UsuarioService],
     exports: [UsuarioService],
+    controllers:[
+        UsuarioController
+    ]
 })
 export class UsuarioModule {
 }
